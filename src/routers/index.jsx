b9c3950154ef766272012d041e9c2fa3c773486a
Armando Pratama <armandopratama0524@gmail.com>
2025-01-04 19:41:31 +0700

    Halaman About

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/index"
+import About from "../pages/about";
 
 
 export const router = createBrowserRouter([
     {
         path: `/`,
         element: <Home />,
+    },
+
+    {
+        path: "/about",
+        element: <About />
     },
 ]) 
 
